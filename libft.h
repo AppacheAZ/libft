afeb13e9cdf0d412c67a2cb8a80362b511ea7e44
AppacheAZ <marcoaz2001@outlook.com>
2023-07-29 12:48:55 +0200

    Añado las funciones al proyecto

--- libft.h
+++ libft.h
@@ -32,4 +32,6 @@ void			*ft_calloc(size_t count, size_t size);
 char			*ft_strdup(const char *s);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strtrim(char const *s1, char const *set);
+char			*ft_itoa(int n);
 #endif
